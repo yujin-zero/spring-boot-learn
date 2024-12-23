432562ed2f3b23b1234075d27505bd25dc551e24
yujin-zero <poly30@naver.com>
2024-12-24 05:26:28 +0900

    add: 회원 리포지토리 테스트 케이스 작성

---
+++ src/test/java/hello/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,60 @@
+package hello.hello_spring.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.hello_spring.domain.Member;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertEquals(result, member);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+        assertEquals(result,member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertEquals(result.size(),2);
+    }
+
+}
