e8e40e99cb62a86b7fd3f1034e18d1de2fcf102b
yujin-zero <poly30@naver.com>
2024-12-27 11:01:37 +0900

    add: 자바 코드로 직접 스프링 빈 등록하기
    MemberServicie와 MemberRepository의 애노테이션 제거
    SpringConfig로 스프링 빈 등록

--- src/main/java/hello/hello_spring/repository/MemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
